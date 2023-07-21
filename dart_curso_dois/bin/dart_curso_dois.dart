8978b5de963a4cae9e44a4c47646cdfe4e2fa07a
Americo <gabriel.americo@herologin.com>
2023-07-21 13:47:08 -0300

    1.1

--- dart_curso_dois/bin/dart_curso_dois.dart
+++ dart_curso_dois/bin/dart_curso_dois.dart
@@ -1,3 +1,18 @@
 void main(List<String> arguments) {
-  print("Oi, Mundo");
+  String nome = "Laranja";
+  double peso = 100.2;
+  String cor = "Verde e Amarela";
+  String sabor = "Doce e citrica";
+  int diasDesdeColheita = 40;
+  bool isMadura = funcEstaMadura(diasDesdeColheita);
+
+  print(isMadura);
+}
+
+bool funcEstaMadura(int dias){
+  if(dias >= 30){
+  return true;
+  }else{
+    return false;
+  }
 }
