7a1e2b0ab27c10e8c3d6da486b9e0fa413bce7e8
Americo <gabriel.americo@herologin.com>
2023-07-26 14:30:50 -0300

    1.4

--- bin/dart_curso_dois.dart
+++ bin/dart_curso_dois.dart
@@ -3,10 +3,52 @@ void main(List<String> arguments) {
   double peso = 100.2;
   String cor = "Verde e Amarela";
   String sabor = "Doce e citrica";
-  int diasDesdeColheita = 40;
+  int diasDesdeColheita = 20;
   bool isMadura = funcEstaMadura(diasDesdeColheita);
 
-  print(isMadura);
+  mostarMadura("uva", 40, cor: "Roxa");
+  int quantosDias = funcQuantosDiasMadura(diasDesdeColheita);
+  print(quantosDias);
+}
+
+ int funcQuantosDiasMadura(int dias){
+  int diasParaMadura = 30;
+  int quantosDiasFaltam = diasParaMadura - dias;
+  return quantosDiasFaltam;
+}
+
+//Parametros
+/*Posicionais obrigatorios, devem ser posicionados na ordem
+ que sao setados na construção da função
+e sao obrigatorios na chamada da função*/
+
+/*Nomeados opcionais
+Sao parametros que nao sao essenciais na função porem podem ser informados na chamada dela
+deve-se passar o argumento com nome do parametro dois ponto e o valor do argumento 
+EX: cor: "roxa", tambem podem ser colocados em qualquer opção
+*/
+
+/*Parametros com "Padrao"
+Esse parametro é definido com = no parametro, e quando é colocado,
+verifica se existe um valor passado no argumento,
+caso nao seja, ele preenche com o default definido no parametro da função
+*/
+
+/*Modificador Required
+Ele é um parametro obrigatorio, porem é nomeado, o que torna mais simples
+ a vizualição de qual argumento vai para qual parametro
+*/
+mostarMadura (String nome, int dias, {required String cor}){
+  if(dias >=30){
+    print("A $nome está madura");
+  }else{
+        print("A $nome não está madura");
+
+  }
+  if(cor != null){
+    print("A $nome é $cor");
+  }
+
 }
 
 bool funcEstaMadura(int dias){
@@ -16,3 +58,4 @@ bool funcEstaMadura(int dias){
     return false;
   }
 }
+
